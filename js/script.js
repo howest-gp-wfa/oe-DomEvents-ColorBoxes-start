5be4979f2fb45eb80d7ac3a0a9bde2a914651b25
mileto1975 <mileto.di.marco@howest.be>
2020-02-11 14:16:53 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//declare global vars here
+
+
+//wait for document load
+window.addEventListener('load',Initialize);
+
+function Initialize()
+{
+   //bind the elements
+   
+   //register eventhandlers
+   
+  
+}
+/*
+Gebruik onderstaande functies als eventhandlers. deze functies moeten
+de class attribute van het HTML element veranderen door gebruik te maken van de
+css classes grid-item(originele toestand) en color-black(zwarte kleur)
+*/
+function ChangeColorToBlack()
+{
+   
+}
+function ResetColor()
+{
+   
+}
+
+
+
